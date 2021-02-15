8a01a629581142ca07a36c59fbaac126a5000ac8
Forellko <dragonforce97q@gmail.com>
2021-02-15 12:26:20 +0300

    sign in up controllers

--- back/controllers/controllerSignIn.js
+++ back/controllers/controllerSignIn.js
@@ -1,6 +1,46 @@
 const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcrypt');
+const { user } = require('../models');
 
-const controllerSignIn = (req, res) => {
+const controllerSignIn = async (req, res) => {
+  let { email, password } = req.body;
+
+  try {
+    const currentUser = await user.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!currentUser) {
+      console.log({ __filename, error: 'User not found' });
+      return res.status(400).json({ __filename, error: 'User not found' });
+    }
+
+    const hashedPassword = currentUser.password;
+
+    const isCorrectPassword = await bcrypt.compare(password, hashedPassword);
+
+    if (!isCorrectPassword) {
+      console.log({ __filename, error: 'Wrong password' });
+      return res
+        .status(400)
+        .json({ __filename, error: 'Wrong email or password' });
+    }
+
+    const createdAt = Date.now();
+    const updatedAt = createdAt;
+    await user.create({
+      email,
+      password,
+      createdAt,
+      updatedAt,
+    });
+  } catch (error) {
+    console.log({ __filename, error });
+    return res.status(400).json({ __filename, error });
+  }
+
   res.status(StatusCodes.OK).send('sign in');
 };
 
--- back/controllers/controllerSingUp.js
+++ back/controllers/controllerSingUp.js
@@ -1,7 +1,26 @@
 const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcrypt');
+const { user } = require('../models');
 
-const controllerSingUp = (req, res) => {
-  res.status(StatusCodes.OK).send('sign up');
+const controllerSingUp = async (req, res) => {
+  let { email, password } = req.body;
+
+  try {
+    password = await bcrypt.hash(password, 3);
+
+    const createdAt = Date.now();
+    const updatedAt = createdAt;
+    await user.create({
+      email,
+      password,
+      createdAt,
+      updatedAt,
+    });
+  } catch (error) {
+    console.log({ __filename, error });
+    return res.status(400).json({ __filename, error });
+  }
+  res.status(StatusCodes.OK).send('sing up');
 };
 
 module.exports = controllerSingUp;
